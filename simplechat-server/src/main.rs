d763ea00789295ba6f9ef1c4a4c3536b018407c1
Emil Kelhälä <emil.kelhala@gmail.com>
2025-03-15 18:29:39 +0200

    Implemented client rejection when reaching server.max_clients

--- simplechat-server/src/main.rs
+++ simplechat-server/src/main.rs
@@ -11,12 +11,19 @@ fn main() {
     let clients:ClientsCollection = Arc::new(Mutex::new(HashMap::new()));
     let config = config_parser::load_config();
     let listener = server::start_server(config.server.bind_port, config.server.bind_interface);
-    println!("[INFO] Listening for connections at {}", config.server.bind_interface);
+    println!("[INFO] Listening for connections at {}:{}", 
+            config.server.bind_interface,
+            config.server.bind_port);
     loop {
         match listener.accept() {
-            Ok((stream, _addr)) => {
-                let clients_collection = Arc::clone(&clients);
-                thread::spawn(move || clients::handle_connection(stream, clients_collection));
+            Ok((stream, addr)) => {
+                if clients.lock().unwrap().len() < config.server.max_clients.into() {
+                    let clients_collection = Arc::clone(&clients);
+                    thread::spawn(move || clients::handle_connection(stream, clients_collection));
+                }
+                else {
+                    println!("[INFO] Rejected client {addr}, because exceeded server.max_clients")
+                }
             },
             Err(error) => println!("[ERROR] Failed accepting client connection: {error:?}"),
         }
